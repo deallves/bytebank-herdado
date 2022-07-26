6761a0d8015ab6f4ebbcf511b7346ecf1da37c06
deallves <deallves@gmail.com>
2022-07-25 22:48:58 -0300

    herança

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,17 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		nico.setNome("nico steppat");
+		nico.setCpf("222222222-1");
+		nico.setSalario(2600.00);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+		
+
+	}
+
+}
