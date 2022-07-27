e7c69831a26463e7f4ede9b64d55e0cfd27bc850
deallves <deallves@gmail.com>
2022-07-26 22:18:10 -0300

    Trabalhando com metodos

--- src/FuncionarioTeste.java
+++ src/FuncionarioTeste.java
@@ -4,12 +4,41 @@ public class FuncionarioTeste {
 	private String nome;
 	private String cpf;
 	private double salario;
+	private int tipo = 0; 
+	private int senha;
 	
+	
+	
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	} 
 	
 	public double getBonificacao() {
-		return this.salario * 0.1;
-	}
+		
+		
+		
+		if(this.tipo == 0) {
+			return this.salario * 0.1;			
+		}else if(this.tipo == 1) {
+			return this.salario;
+		}else {
+		    return this.salario + 1000.0;
+		    }
+		    }
+		
 	
+		public void setTipo(int tipo) {
+			this.tipo = tipo;
+		}
+		public int getTipo() {
+			return tipo;
+		
+		}
 	
 	
 	public String getNome() {
