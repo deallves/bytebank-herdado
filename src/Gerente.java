62cfdb01507d5cb5c498df604b1663f58ba91de6
deallves <deallves@gmail.com>
2022-08-04 21:25:49 -0300

    classe abstract

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,23 +1,10 @@
 
 //Gerente e um funcionario herda da class funcionario
-public class Gerente extends Funcionario {
+public class Gerente extends FuncionarioAutenticavel {
 	
 	
-	private int senha;
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	
-	public boolean autentic(int senha) {
-		if(this.senha == senha) {
-			return true;
-		}else {
-			return false;
-		}
 		
-	}
+	
 	
 	public double getBonificacao() {
 		System.out.println("chamando o metodo de bonoficação GERENTE ");
